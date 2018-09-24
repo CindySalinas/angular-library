dcb301cbfcb4e520e35560709fe2a15e407638c4
Cindy Salinas <cindysalinas15@gmail.com>
2018-09-24 17:14:38 -0500

    add login service

--- src/login.module.ts
+++ src/login.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Login } from './login';
+import { LoginService } from './login.service';
 @NgModule({
   declarations: [
     Login,
+    LoginService
   ],
   exports: [
     Login,
-  ],
+    LoginService
+  ]
 })
 export class LoginModule { }
